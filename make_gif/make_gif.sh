c6c95f0a72ae2d4b090bfa18951664961fc61d3b
Daniele Ongari <danieleongari@Danieles-MacBook-Pro.local>
2018-10-09 22:49:44 +0200

    corrected make_gif

--- make_gif/make_gif.sh
+++ make_gif/make_gif.sh
@@ -3,11 +3,11 @@
 rm *.png
 rm ../RENAME.gif
 
-gnuplot psisq_gif.gnuplot
+gnuplot psi_gif.gnuplot
 
 for i in $(seq 0 9)
 do
-mv psisq_${i}.png psisq_0${i}.png
+mv psi_${i}.png psi_0${i}.png
 done
 
 convert -delay 5 -loop 0 *.png ../REDAME.gif
