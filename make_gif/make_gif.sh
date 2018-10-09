aa89cd1de23a95466b61c23d527b6d5af91ea8f7
Daniele Ongari <danieleongari@Danieles-MacBook-Pro.local>
2018-10-09 22:51:59 +0200

    small mistake

--- make_gif/make_gif.sh
+++ make_gif/make_gif.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm *.png
-rm ../RENAME.gif
+rm ../README.gif
 
 gnuplot psi_gif.gnuplot
 
@@ -10,4 +10,4 @@ do
 mv psi_${i}.png psi_0${i}.png
 done
 
-convert -delay 5 -loop 0 *.png ../REDAME.gif
+convert -delay 5 -loop 0 *.png ../README.gif
